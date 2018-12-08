c008912e3c2859c0232fc9e1c1f82af949d6ea7d
Marcos <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-08 12:20:26 +0530

    Add files via upload

---
+++ ajit.py
@@ -0,0 +1,20 @@
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+client = Bot(description="BOT DESCRIPTION HERE", command_prefix="BOT PREFIX HERE", pm_help = False) #add_your_bot_description_with_prefix_here
+
+@client.event
+async def on_ready():
+	print('Logged in as '+client.user.name+'')
+	print('--------')
+	print('--------')
+	print('Started <BOTNAME HERE>') #add_your_bot_name_here
+	return await client.change_presence(game=discord.Game(name='<BOT STATUS HERE>')) #add_your_bot_status_here
+
+@client.command()
+async def ping(*args): #replace_ping_from_any_other_command_name_like_hu,hello,etc
+	await client.say("COMMAND RESPONCE HERE") #replace_responce_with_any_responce_that__you_want_when_command_is_used
+
+client.run(os.getenv('Token'))
